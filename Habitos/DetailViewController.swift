1b6ad71fa0c0587fefc883e15404f6a3dfc6d31b
Manu Mateos <manu.mateos@tecnilogica.com>
2017-08-11 14:22:11 +0200

    Listing de eventos

--- Habitos/DetailViewController.swift
+++ Habitos/DetailViewController.swift
@@ -12,6 +12,13 @@ class DetailViewController: UIViewController {
     
     var viewModel = DetailViewModel()
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var todayLabel: UILabel!
+    @IBOutlet weak var thisWeekLabel: UILabel!
+    @IBOutlet weak var thisMonthLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     func configureNavigation() {
         var titleForController = ""
         
@@ -22,11 +29,22 @@ class DetailViewController: UIViewController {
         self.navigationController?.navigationItem.title = titleForController
         
     }
+    
+    func configureView() {
+        totalLabel.text = "Total: \(viewModel.events.count)"
+        todayLabel.text = "Hoy: \(viewModel.today)"
+        thisWeekLabel.text = "Esta semana: \(viewModel.thisWeek)"
+        thisMonthLabel.text = "Este mes: \(viewModel.thisMonth)"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         configureNavigation()
+        configureView()
     }
 
 //    var detailItem: Habit? {
@@ -35,7 +53,33 @@ class DetailViewController: UIViewController {
 //            configureNavigation()
 //        }
 //    }
-
-
+    
 }
+
+extension DetailViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.events.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let event = viewModel.events[indexPath.row]
+        configureCell(cell: cell, withEvent: event)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    func configureCell(cell: UITableViewCell, withEvent event: Event) {
+        cell.textLabel!.text = event.timestamp?.description
+    }
+}
+
 
--- Habitos/DetailViewModel.swift
+++ Habitos/DetailViewModel.swift
@@ -11,5 +11,42 @@ import UIKit
 class DetailViewModel: NSObject {
     var habit: Habit?
     
+    var today: Int {
+        let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
+        let todayComponents = calendar.dateComponents([.day, .month, .year], from: Date())
+        let todayDate = calendar.date(from: todayComponents)!
+//        let aWeekAgoDate = calendar.date(byAdding: .day, value: -7, to: todayDate)
+        
+        let filterDayPredicate = NSPredicate(format: "timestamp >= %@", argumentArray: [todayDate])
+        let todayEvents = habit!.events!.filtered(using: filterDayPredicate)
+//        let filterWeekPredicate = NSPredicate(format: "timestamp >= %@", argumentArray: [aWeekAgoDate])
+//        let weekEvents = habit!.events!.filtered(using: filterWeekPredicate)
+        return todayEvents.count
+    }
+    var thisWeek: Int {
+        let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
+        let todayComponents = calendar.dateComponents([.day, .month, .year], from: Date())
+        let todayDate = calendar.date(from: todayComponents)!
+        let aWeekAgoDate = calendar.date(byAdding: .day, value: -7, to: todayDate)
+        
+        let filterWeekPredicate = NSPredicate(format: "timestamp >= %@", argumentArray: [aWeekAgoDate])
+        let weekEvents = habit!.events!.filtered(using: filterWeekPredicate)
+        return weekEvents.count
+    }
+    var thisMonth: Int {
+        let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
+        let todayComponents = calendar.dateComponents([.day, .month, .year], from: Date())
+        var firstDayOfMonthComponents = todayComponents
+        firstDayOfMonthComponents.day = 1
+        let firstDayOfMonth = calendar.date(from: firstDayOfMonthComponents)
+        
+        let filterWeekPredicate = NSPredicate(format: "timestamp >= %@", argumentArray: [firstDayOfMonth])
+        let monthEvents = habit!.events!.filtered(using: filterWeekPredicate)
+        return monthEvents.count
+    }
+    var events: [Event] {
+        return habit!.events?.allObjects as! [Event]
+    }
+    
     
 }
